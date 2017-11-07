4861e5a0faf4565e1b5262238651938729783680
AhmedSalim <ahmeds.des@gmail.com>
2017-11-07 19:07:46 +0400

    first commit. creating the gui

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,32 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+
+    @FXML
+    Button startAlarm, pauseAlarm, exitAlarm;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+        startAlarm.setOnAction(e->{
+            //start the alarm
+        });
+
+        pauseAlarm.setOnAction(e->{
+            //pause the alarm
+        });
+
+        exitAlarm.setOnAction(e->{
+            //exit the alarm
+        });
+
+    }
 }
